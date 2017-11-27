7a54aeeba1feb18e24f3d62ffc6d849a486ba71d
yuta ito <r.05410scr@gmail.com>
2017-11-27 11:00:43 +0900

    スクロールアニメーション追加

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,24 @@
 $(function(){
-  $("#toggle").click(function(){
+      
+   // #で始まるアンカーをクリックした場合に処理
+   $('.nav-item a[href^=#]').click(function() {
+      // スクロールの速度
+      var speed = 600; // ミリ秒
+      // アンカーの値取得
+      var href= $(this).attr("href");
+      // 移動先を取得
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      // 移動先を数値で取得
+      var position = target.offset().top - 80;
+      // スムーススクロール
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return true;
+   });
+    $("#toggle, .nav-item").click(function(){
     $("#menu").slideToggle();
     return false;
-  });
+});
+    
   $(window).resize(function(){
     var win = $(window).width();
     var p = 480;
